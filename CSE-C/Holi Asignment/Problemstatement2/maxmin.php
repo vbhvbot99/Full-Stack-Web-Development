9b39b882116e630a152fd753f3c607f6c8499ee7
unknown <agarwalvaibhav6024@gmail.com>
2020-03-18 14:21:15 +0530

    max and min tasks completed

---
+++ CSE-C/Holi Asignment/Problemstatement2/maxmin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Parse JSON Data</title>
+	<script type="text/javascript" src="jsondata.js"></script>
+</head>
+<body>
+<p><strong><i>ID of all users those have completed maximum number of tasks:</i></strong></p>
+<p id="max"></p><br>
+<p><strong><i>ID of all users those have completed minimum number of tasks:</i></strong></p>
+<p id="min"></p>
+<script type="text/javascript" > 
+	document.getElementById("max").innerHTML = trueid;
+	document.getElementById("min").innerHTML = falseid;
+</script>
+</body>
+</html>
