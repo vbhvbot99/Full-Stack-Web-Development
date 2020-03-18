67732617d83a750ec9b9e4504d8106ef9b29c93d
unknown <agarwalvaibhav6024@gmail.com>
2020-03-18 14:10:39 +0530

    add details

---
+++ CSE-C/Holi Asignment/Problemstatement1/add1.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+    <title>Input Form</title>
+    </head>
+    
+    <body>
+    <form action="addrecord1.php" method="post">
+      Enrolment: <input type="text" name="enrollment" placeholder="Enter enrollment"><br>
+      Name: <input type="text" name="username"><br>
+      Age:<input type="text" name="age" placeholder="Enter Age"><br>
+      Course:<input type="text" name="course" placeholder="Enter Course"><br>
+      Branch: <input type="text" name="branch" placeholder="Enter Branch"><br>
+      Contact: <input type="text" name="contact" placeholder="Enter Contact"><br>
+      Email: <input type="text" name="email" placeholder="Enter Email"><br>
+      <input type="submit" name='btn'>
+    </form>
+    </body>
+</html>
